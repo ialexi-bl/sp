463cc24841971736f88f89217bc97687b40a74af
Aleksei Chashin <jabuj@yandex-team.ru>
2022-05-30 15:18:05 +0300

    fixup! fixup! link href

--- index.js
+++ index.js
@@ -188,7 +188,7 @@
 
                 /** @type {any} */
                 let node = event.target
-                while (node || !(node instanceof HTMLAnchorElement) || !`${BASE}${node.href}`.endsWith(historyEntry)) {
+                while (node && node instanceof HTMLAnchorElement && !`${BASE}${node.href}`.endsWith(historyEntry)) {
                     node = node.parentNode
                 }
 
